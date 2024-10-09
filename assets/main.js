4331bb363cfd24daf75097a19ae195a1b0af92eb
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-09 16:05:23 +0200

    docs: fill html form, comments and human code

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,15 @@
 console.log('im alive');
+
+const form_element = document.querySelector('form');
+console.log(form_element);
+
+// cosa c'è in più dell'es di prima?
+// output di tutti i dati del form
+// tipo di prezzo
+// n° carrozza e codice cp (n° random, funzioni?)
+
+// passi:
+// collegare gli elementi del form e dell'output al file js
+// creare delle funzioni che prendano gli input del form
+// calcolare i dati del form con una funzione
+// stampare nell'output

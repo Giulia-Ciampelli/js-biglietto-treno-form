34b4f68f5127ebf47562baa8c28f167e11c99e02
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-09 16:30:38 +0200

    docs: variable declaration

--- assets/main.js
+++ assets/main.js
@@ -11,5 +11,13 @@ console.log(form_element);
 // passi:
 // collegare gli elementi del form e dell'output al file js
 // creare delle funzioni che prendano gli input del form
+// collegare gli eventi al click
 // calcolare i dati del form con una funzione
-// stampare nell'output
+// stampare nell'output (con document.getElementById('id')?)
+
+// dichiarazione variabili
+let basePrice;
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let distance = document.getElementById('kms');
+let age = document.getElementById('yrs');

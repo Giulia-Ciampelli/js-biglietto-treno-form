afd8e3d5eab4576cc17a94aef307c22e7d6a8a3c
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-09 18:05:56 +0200

    docs: print type of price

--- assets/main.js
+++ assets/main.js
@@ -20,7 +20,7 @@ let userDistance = document.getElementById('kms');
 let userAge = document.getElementById('yrs');
 console.log(userName, userDistance, userAge);
 
-// funzione:
+// funzioni random per carrozza e codice cp da richiamare dentro l'evento
 
 // evento per premuta tasto
 formElement.addEventListener('submit', (e) => {
@@ -31,16 +31,24 @@ formElement.addEventListener('submit', (e) => {
     const inputDistance = userDistance.value;
     const inputAge = userAge.value;
     
-    // calcolo prezzo per km, da mettere in una funzione
+    // calcolo prezzo per km
     basePrice = inputDistance * 0.21;
     
     // applicazione sconto
     if (inputAge < 18) {
         basePrice = basePrice - (basePrice * 0.2);
+        document.getElementById('out-offer').innerHTML = 'Biglietto ridotto - minorenni';
     }
     else if (inputAge >= 65) {
         basePrice = basePrice - (basePrice * 0.4);
+        document.getElementById('out-offer').innerHTML = 'Biglietto ridotto - pensionati';
     }
-    
+    else {
+        document.getElementById('out-offer').innerHTML = 'Biglietto standard';
+    }
     console.log(inputName, inputAge, inputDistance, basePrice);
+
+    // stampa nell'output
+    document.getElementById('out-name').innerHTML = inputName;
+    document.getElementById('out-price').innerHTML = `${basePrice.toFixed(2)}€`;
 })

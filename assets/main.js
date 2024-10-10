c7b5d29b9878683727ff62a7e82328f14884d40b
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-10 10:41:34 +0200

    docs: add class remove function to only show the output after submit

--- assets/main.js
+++ assets/main.js
@@ -6,9 +6,11 @@ let userDistance = document.getElementById('kms');
 let userAge = document.getElementById('yrs');
 // #endregion dichiarazione variabili
 
-// #region funzioni random per carrozza e codice cp da richiamare dentro l'evento
+// #region funzioni da richiamare dentro l'evento
+
+// funzioni random per carrozza e codice cp
 function randomSeat() {
-    let seat = Math.floor((Math.random() * 20) +1);
+    let seat = Math.floor((Math.random() * 20) + 1);
     document.getElementById('out-random-1').innerHTML = seat;
 }
 
@@ -16,20 +18,29 @@ function randomId() {
     let codex = Math.floor((Math.random() * 99999) + 11111);
     document.getElementById('out-random-2').innerHTML = codex;
 }
-// #endregion funzioni random per carrozza e codice cp da richiamare dentro l'evento
+
+// funzione per mostrare l'output al submit
+function classRemove() {
+    let output = document.getElementById('result');
+    output.classList.remove('d-none');
+}
+// #endregion funzioni da richiamare dentro l'evento
 
 // #region evento per premuta tasto
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
+    // rimozione o toggle classe per mostrare la regione output
+    classRemove();
+
     // dichiarazione variabili di valore input
     const inputName = userName.value;
     const inputDistance = userDistance.value;
     const inputAge = userAge.value;
-    
+
     // calcolo prezzo per km
     basePrice = inputDistance * 0.21;
-    
+
     // applicazione sconto
     if (inputAge < 18 && inputAge > 0) {
         basePrice = basePrice - (basePrice * 0.2);
